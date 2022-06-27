f38664d551c718f6b25bcc176b68c68a9a97e3c5
ndev21 <nick@leonotes.org>
2022-06-26 20:20:01 -0600

    Add code for print player age and name

--- player/player.go
+++ player/player.go
@@ -12,6 +12,14 @@ func NewPlayer(name string, number int, age int) Player {
 	return Player{name: name, number: number, age: age}
 }
 
+func AgeString(player Player) string {
+	return fmt.Sprintf("Player Age: %d", player.age)
+}
+
+func NameString(player Player) string {
+	return fmt.Sprintf("Player Name: %s", player.name)
+}
+
 func PrintName(player Player) {
 	fmt.Printf("Player Name: %s\n", player.name)
 }
